e705e81bb5b3df68894724c1e80b5ede26289ac9
Farias123 <vitorcostafarias@gmail.com>
2023-03-05 12:12:34 -0300

    Merge functions
    I merged the functions into one, with a new definer parameter to identify them.

--- gauss_functions.py
+++ gauss_functions.py
@@ -1,52 +1,62 @@
-def obterCoefX(eq):
+def getCoefX(eq):
 #Essa funcao vai percorrer os caracteres da equacao e pegar um por um ate que chegue no x, ela
 #entao retornara o valor do coeficiente multiplicador de X. 
-    coefX = ' '
-    for a in eq:
-        if a != 'x':
-            coefX += str(a)
-        else:
-            break
-    if coefX == ' ':
-        coefX = 1
-    elif coefX == ' -':
-        coefX = -1
-    else:
-        coefX = float(coefX)
-    return coefX
+    return 
 
 
-def obterCoefY(eq):
+def getCoefY(eq):
 #Essa funcao vai percorrer os caracteres da equacao depois do sinal de soma e pegar um por um
 #ate que chegue no y, ela entao retornara o valor do coeficiente multiplicador de Y. 
-    contSomas = 0
-    coefY = ''
-    for a in eq:
-        if contSomas == 0:
-            if a == '+':
-                contSomas += 1
-        elif a != 'y':
-            coefY += str(a)
-        else:
-            break
-    if coefY == ' ':
-        coefY = 1
-    elif coefY == ' -':
-        coefY = -1
-    else:
-        coefY = float(coefY)
-    return coefY
+    return 
 
-def obterIndep(eq):
+def getIndep(eq):
 #Essa funcao vai percorrer os caracteres da equacao depois do sinal de igualdade e pegar um por um
 #ate que chegue ao fim da equacao, ela entao retornara o valor do termo independente. 
-    foiIgual = False
-    termoIndep = ''
-    for a in eq:
-        if foiIgual == False:
-            if a == '=':
-                foiIgual = True
+    return 
+
+def getCoef(eq,definer):
+    if definer == "X":
+        coefX = ' '
+        for a in eq:
+            if a != 'x':
+                coefX += str(a)
+            else:
+                break
+        if coefX == ' ':
+            coefX = 1
+        elif coefX == ' -':
+            coefX = -1
         else:
-            termoIndep += str(a)
-    termoIndep = float(termoIndep)
-    return termoIndep
+            coefX = float(coefX)
+        return coefX
+    
+    elif definer == "Y":
+        sumHappened = False
+        coefY = ''
+        for a in eq:
+            if sumHappened == False:
+                if a == '+':
+                    sumHappened = True
+            elif a != 'y':
+                coefY += str(a)
+            else:
+                break
+        if coefY == ' ':
+            coefY = 1
+        elif coefY == ' -':
+            coefY = -1
+        else:
+            coefY = float(coefY)
+        return coefY
+    
+    elif definer == "Indep":
+        equalsHappened = False
+        termIndep = ''
+        for a in eq:
+            if equalsHappened == False:
+                if a == '=':
+                    equalsHappened = True
+            else:
+                termIndep += str(a)
+        termIndep = float(termIndep)
+        return termIndep
